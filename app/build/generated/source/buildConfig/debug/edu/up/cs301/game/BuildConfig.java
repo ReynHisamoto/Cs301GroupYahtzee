f72f07355a52fc9f791da3c8c1b58a4ef7bb0009
amnuxoll <amnuxoll@hotmail.com>
2019-10-03 14:36:58 -0700

    Update to newer version of android studio and associated build tools

---
+++ app/build/generated/source/buildConfig/debug/edu/up/cs301/game/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.up.cs301.game;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "edu.up.cs301.game";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ app/src/main/java/edu/up/cs301/pig/PigComputerPlayer.java
@@ -0,0 +1,34 @@
+package edu.up.cs301.pig;
+
+import edu.up.cs301.game.GameComputerPlayer;
+import edu.up.cs301.game.actionMsg.GameAction;
+import edu.up.cs301.game.infoMsg.GameInfo;
+import edu.up.cs301.game.util.Tickable;
+
+/**
+ * An AI for Pig
+ *
+ * @author Andrew M. Nuxoll
+ * @version August 2015
+ */
+public class PigComputerPlayer extends GameComputerPlayer {
+
+    /**
+     * ctor does nothing extra
+     */
+    public PigComputerPlayer(String name) {
+        super(name);
+    }
+
+    /**
+     * callback method--game's state has changed
+     *
+     * @param info
+     * 		the information (presumably containing the game's state)
+     */
+    @Override
+    protected void receiveInfo(GameInfo info) {
+        // TODO  You will implement this method
+    }//receiveInfo
+
+}
