9345bc49db8fcbf529570d5f7751352275a23b9e
pham24 <pham24@up.edu>
2022-03-29 09:17:53 -0700

    quality of life update

--- app/src/main/java/edu/up/cs301/pig/YahtzeeHumanPlayer.java
+++ app/src/main/java/edu/up/cs301/pig/YahtzeeHumanPlayer.java
@@ -101,7 +101,7 @@ public class YahtzeeHumanPlayer extends GameHumanPlayer implements OnClickListen
                  scores[i][j].setText(((YahtzeeGameState)info).getScores(i)[j]+ "");
              }
          }
-         roundNum.setText("ROUND: " + ((YahtzeeGameState) info).getRound());
+         roundNum.setText("ROUND: " + ((YahtzeeGameState) info).getRound() + "\n" + allPlayerNames[((YahtzeeGameState) info).getTurn()] + "'s Turn");
         }
     }//receiveInfo
 
